c61596505fd5fb49a636ed5cb0d5c7ab638c986f
Francesco <francesco@fulminlabs.org>
2023-07-07 22:48:59 +0200

    fix(nonce-finder): bug fix + edge cases
    Signed-off-by: Francesco <francesco@fulminlabs.org>

--- src/Permit2NonceFinder.sol
+++ src/Permit2NonceFinder.sol
@@ -1,23 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.17;
 
 import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
 
 contract Permit2NonceFinder {
     ISignatureTransfer public immutable permit2;
 
+    /// @notice Constructs the Permit2NonceFinder contract
+    /// @param _permit2 The address of the Permit2 contract
     constructor(address _permit2) {
         permit2 = ISignatureTransfer(_permit2);
     }
 
-    function nextNonce(address _owner) external view returns (uint256 nonce) {
-        nonce = _nextNonce(_owner, 0, 0);
+    /// @notice Finds the next valid nonce for a user, starting from 0.
+    /// @param owner The owner of the nonces
+    /// @return nonce The first valid nonce starting from 0
+    function nextNonce(address owner) external view returns (uint256 nonce) {
+        nonce = _nextNonce(owner, 0, 0);
     }
 
+    /// @notice Finds the next valid nonce for a user, after from a given nonce.
+    /// @dev This can be helpful if you're signing multiple nonces in a row and need the next nonce to sign but the start one is still valid.
+    /// @param owner The owner of the nonces
+    /// @param start The nonce to start from
+    /// @return nonce The first valid nonce after the given nonce
     function nextNonce(address owner, uint256 start) external view returns (uint256 nonce) {
-        nonce = _nextNonce(owner, uint248(start >> 8), uint8(start));
+        uint248 word = uint248(start >> 8);
+        uint8 pos = uint8(start);
+        if (pos == type(uint8).max) {
+            // If the position is 255, we need to move to the next word
+            word++;
+            pos = 0;
+        } else {
+            // Otherwise, we just move to the next position
+            pos++;
+        }
+        nonce = _nextNonce(owner, word, pos);
     }
 
+    /// @notice Finds the next valid nonce for a user, starting from a given word and position.
+    /// @param owner The owner of the nonces
+    /// @param word Word to start looking from
+    /// @param pos Position inside the word to start looking from
     function _nextNonce(address owner, uint248 word, uint8 pos) internal view returns (uint256 nonce) {
         while (true) {
             uint256 bitmap = permit2.nonceBitmap(owner, word);
@@ -25,19 +49,28 @@ contract Permit2NonceFinder {
             // Check if the bitmap is completely full
             if (bitmap == type(uint256).max) {
                 // If so, move to the next word
-                word++;
+                ++word;
+                pos = 0;
                 continue;
+            }
+            if (pos != 0) {
+                // If the position is not 0, we need to shift the bitmap to ignore the bits before position
+                bitmap = bitmap >> pos;
             }
             // Find the first zero bit in the bitmap
             while (bitmap & 1 == 1) {
                 bitmap = bitmap >> 1;
-                pos++;
+                ++pos;
             }
 
             return _nonceFromWordAndPos(word, pos);
         }
     }
 
+    /// @notice Constructs a nonce from a word and a position inside the word
+    /// @param word The word containing the nonce
+    /// @param pos The position of the nonce inside the word
+    /// @return nonce The nonce constructed from the word and position
     function _nonceFromWordAndPos(uint248 word, uint8 pos) internal pure returns (uint256 nonce) {
         // The last 248 bits of the word are the nonce bits
         nonce = uint256(word) << 8;
---
+++ test/Permit2NonceFinder.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Permit2NonceFinder} from "src/Permit2NonceFinder.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+import {DeployPermit2} from "permit2/test/utils/DeployPermit2.sol";
+
+contract Permit2NonceFinderTest is Test, DeployPermit2 {
+    Permit2NonceFinder internal finder;
+    ISignatureTransfer internal permit2;
+
+    function setUp() public {
+        permit2 = ISignatureTransfer(deployPermit2());
+        finder = new Permit2NonceFinder(address(permit2));
+    }
+
+    function test_findNonce() public {
+        // We invalidate the first nonce to make sure it's not returned.
+        // We pass a mask of 0...0011 to invalidate nonce 0 and 1.
+        permit2.invalidateUnorderedNonces(0, 3);
+        assertEq(finder.nextNonce(address(this)), 2);
+
+        // Invalidate the first word minus 1 nonce
+        permit2.invalidateUnorderedNonces(0, type(uint256).max >> 1);
+        // We should find the last nonce in the first word
+        assertEq(finder.nextNonce(address(this)), 255);
+    }
+
+    function test_findNextAfterFromStart() public {
+        // We want to start from the second word
+        uint256 start = 256;
+        // We invalidate the whole next word to make sure it's not returned.
+        permit2.invalidateUnorderedNonces(1, type(uint256).max);
+        assertEq(finder.nextNonce(address(this), start), 512);
+
+        // Invalidate the next word minus 1 nonce
+        permit2.invalidateUnorderedNonces(2, type(uint256).max >> 1);
+        // We should find the first nonce in the third word
+        assertEq(finder.nextNonce(address(this), 767), 768);
+
+        // The first word is still accessible if we start from a lower nonce
+        assertEq(finder.nextNonce(address(this), 1), 2);
+    }
+}
